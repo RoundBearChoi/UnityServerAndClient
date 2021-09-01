f9b45d1737807e6ca6c77bc5c1b3b09771c4e599
RBG <roundbeargames@gmail.com>
2021-09-01 11:55:10 +0900

    start

---
+++ ServerAndClient_clone_0/Assets/Network/Client/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RB.Client
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            SendInputToServer();
+        }
+
+        /// <summary>Sends player input to the server.</summary>
+        private void SendInputToServer()
+        {
+            bool[] _inputs = new bool[]
+            {
+            Input.GetKey(KeyCode.W),
+            Input.GetKey(KeyCode.S),
+            Input.GetKey(KeyCode.A),
+            Input.GetKey(KeyCode.D),
+            Input.GetKey(KeyCode.Space)
+            };
+
+            ClientSend.PlayerMovement(_inputs);
+        }
+    }
+}
