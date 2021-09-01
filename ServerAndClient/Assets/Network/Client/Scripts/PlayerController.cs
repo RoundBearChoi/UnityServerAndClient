980579441e01a56b0127609455cccd0b1e54620b
RBG <roundbeargames@gmail.com>
2021-09-01 20:29:21 +0900

    input serialize

--- ServerAndClient/Assets/Network/Client/Scripts/PlayerController.cs
+++ ServerAndClient/Assets/Network/Client/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ namespace RB.Client
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField]
+        bool[] _inputs = null;
+
         private void FixedUpdate()
         {
             SendInputToServer();
@@ -14,14 +17,16 @@ namespace RB.Client
         /// <summary>Sends player input to the server.</summary>
         private void SendInputToServer()
         {
-            bool[] _inputs = new bool[]
+            if (_inputs == null || _inputs.Length == 0)
             {
-            Input.GetKey(KeyCode.W),
-            Input.GetKey(KeyCode.S),
-            Input.GetKey(KeyCode.A),
-            Input.GetKey(KeyCode.D),
-            Input.GetKey(KeyCode.Space)
-            };
+                _inputs = new bool[5];
+            }
+
+            _inputs[0] = Input.GetKey(KeyCode.W);
+            _inputs[1] = Input.GetKey(KeyCode.S);
+            _inputs[2] = Input.GetKey(KeyCode.A);
+            _inputs[3] = Input.GetKey(KeyCode.D);
+            _inputs[4] = Input.GetKey(KeyCode.Space);
 
             ClientSend.PlayerMovement(_inputs);
         }
